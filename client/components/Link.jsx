19fe785446972ded1fd56a2d81555f59ab4a4769
Humanity <jinscha@indiana.edu>
2016-07-27 12:01:58 -0700

    Make link URL dynamic to dev or prod

--- client/components/Link.jsx
+++ client/components/Link.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+let host = process.env.NODE_ENV === 'production' ? 'https://realtimeapp.herokuapp.com/' : 'http://localhost:3000/'; 
 const Link = (props) => (
   <div id="link">
     <div id="link-message">
       Send your friend the following link:<br />
-      <span id="link-url">http://localhost:3000/?id={props.myId}</span>
+      <span id="link-url">{host}?id={props.myId}</span>
     </div>
   </div>
 );
