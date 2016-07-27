bd3577342a4308ef29077d501379978ae153ac5f
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-27 16:23:40 -0700

    Implement copy-to-clipboard proof of concept for shareable link

--- client/components/Link.jsx
+++ client/components/Link.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 let host = process.env.NODE_ENV === 'production' ? 'https://reeltimeapp.herokuapp.com/' : 'http://localhost:3000/'; 
-const Link = (props) => (
-  <div id="link">
-    <div id="link-message">
-      Send your friend the following link:<br />
-      <span id="link-url">{host}?id={props.myId}</span>
+
+const Link = (props) => {
+  let url = host + '?id=' + props.myId;
+
+  return (
+    <div id="link">
+      <div id="link-message">
+        Send your friend the following link:<br />
+        <span id="link-url">{url}</span>
+        <CopyToClipboard text={`${url}`}>
+          <button>Copy to clipboard</button>
+        </CopyToClipboard>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Link.propTypes = {
   myId: React.PropTypes.string,
