b2ddd316990abfa3e57e9a437e97c1e9e6b3a330
Pranay M <pranay@utexas.edu>
2016-07-28 15:55:01 -0700

    implement shoddy youtube sync

--- client/components/YouTubeVideo.jsx
+++ client/components/YouTubeVideo.jsx
@@ -7,34 +7,64 @@ class YouTubeVideo extends React.Component {
 
     this.state = {
       playing: false,
+      played: 0,
+      loaded: 0,
+      duration: 0,
     }
 
     this.emitPlayAndListenForPause = this.emitPlayAndListenForPause.bind(this);
     this.emitPauseAndListenForPlay = this.emitPauseAndListenForPlay.bind(this);
+    this.onProgress = this.onProgress.bind(this);
   }
 
   emitPlayAndListenForPause() {
-    this.props.socket.emit('play', 0);
-    this.props.socket.on('pause', (otherTime) => {
-      this.setState({ playing: false });
+    this.props.socket.emit('play', this.state.played);
+    this.props.socket.on('pause', (otherPlayedFraction) => {
+      console.log('thisplayed:', this.state.played, 'otherplayed:', otherPlayedFraction);
+      this.syncVideos(this.state.played, otherPlayedFraction);
+      this.setState({ 
+        playing: false,
+      });
     });
   }
 
   emitPauseAndListenForPlay() {
-    this.props.socket.emit('pause', 0);
-    this.props.socket.on('play', (otherTime) => {
-      this.setState({ playing: true });
+    this.props.socket.emit('pause', this.state.played);
+    this.props.socket.on('play', (otherPlayedFraction) => {
+      console.log('thisplayed:', this.state.played, 'otherplayed:', otherPlayedFraction);
+      this.syncVideos(this.state.played, otherPlayedFraction);
+      this.setState({ 
+        playing: true,
+      });
     });
+  }
+
+  syncVideos(currentFraction, otherFraction) {
+    var currentTime = Math.floor(currentFraction * this.state.duration);
+    var otherTime = Math.floor(otherFraction * this.state.duration);
+
+    if (currentTime > otherTime + 0.5 || currentTime < otherTime - 0.5) {
+      this.refs.player.seekTo(otherFraction);
+    }
+  }
+
+  onProgress(state) {
+    //TODO save progress
+    this.setState(state);
   }
 
   render () {
     return (
       <ReactPlayer
+        ref='player'
         url={ this.props.url }
         controls
         playing={ this.state.playing }
         onPlay={ this.emitPlayAndListenForPause }
         onPause={ this.emitPauseAndListenForPlay }
+        onProgress = { this.onProgress }
+        progressFrequency = { 250 }
+        onDuration = { duration => this.setState({ duration })}
       />
     )
   }
