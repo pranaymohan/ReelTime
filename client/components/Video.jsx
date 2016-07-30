b4499a7db9e93c23083f9c90b1b8112ecbaaf221
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-07-29 17:13:31 -0700

    Refactor to move initVoice and emitters into VideoWrapper component and pass them down as props to Video component

--- client/components/Video.jsx
+++ client/components/Video.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 class Video extends React.Component {
   constructor(props) {
     super(props);
-
-    this.emitPlayAndListenForPause = this.emitPlayAndListenForPause.bind(this);
-    this.emitPauseAndListenForPlay = this.emitPauseAndListenForPlay.bind(this);
   }
 
   componentDidMount() {
     // Begin animating the video when it starts playing
     const video = document.querySelector('video');
-    this.initVoice(video);
+    this.props.initVoice(video);
     this.initListeners(video);
 
     video.addEventListener('canplay', (e) => {
@@ -37,83 +34,6 @@ class Video extends React.Component {
     });
   }
 
-  // Initialize annyong voice command library and define commands
-  initVoice(video) {
-    var playFunction = () => {
-      console.log('play voice command received');
-      video.play();
-      this.emitPlayAndListenForPause(video);
-    };
-
-    var pauseFunction = () => {
-      console.log('pause voice command received');
-      video.pause();
-      this.emitPauseAndListenForPlay(video);
-    };
-
-    var goBackFunction = () => {
-      console.log('go back voice command received');
-      video.currentTime = Math.floor(video.currentTime - 10, 0);
-      this.emitGoBack(video);
-    };
-
-    var muteFunction = () => {
-      console.log('mute voice command received');
-      video.muted = true;
-    };
-
-    var unmuteFunction = () => {
-      console.log('unmute voice command received');
-      video.muted = false;
-    };
-
-    var playCommands = {'play': playFunction};
-    var goBackCommands = {'go back': goBackFunction};
-
-    // define words that empirically sound like our main command, and synonyms
-    var pauseWords = ['pause', 'call', 'car', 'cars', 'cause', 'hans', 
-      'hollis', 'hot', 'palm', 'paul', 'paula\'s', 'paw', 'pawn', 
-      'paws', 'pod', 'pods', 'polish', 'pond', 'posh'];
-    var muteWords = ['mute', 'new', 'news', 'newt', 'nude', 'use', 'used', 'you'];
-    var unmuteWords = ['unmute', 'enhance', 'enhanced', 'in hands', 'in hats', 'n hance'];
-
-    // generate a command object for each array of command words
-    var pauseCommands = pauseWords.reduce(function(obj, word) {
-      obj[word] = pauseFunction;
-      return obj;
-    }, {});
-
-    var muteCommands = muteWords.reduce(function(obj, word) {
-      obj[word] = muteFunction;
-      return obj;
-    }, {});
-
-    var unmuteCommands = unmuteWords.reduce(function(obj, word) {
-      obj[word] = unmuteFunction;
-      return obj;
-    }, {});
-
-
-    if (annyang) {
-      annyang.debug();
-      annyang.addCommands(playCommands);
-      annyang.addCommands(pauseCommands);
-      annyang.addCommands(goBackCommands);
-      annyang.addCommands(muteCommands);
-      annyang.addCommands(unmuteCommands);
-      annyang.start();
-
-      setInterval(function() {
-        var listen = annyang.isListening();
-        console.log('listen:', listen);
-      }, 1000);
-
-      annyang.addCallback('error', function(err) {
-        console.log('annyang error:', err);
-      });
-    }
-  }
-
   syncVideos(video, otherTime) {
     if (Math.floor(video.currentTime) > Math.floor(otherTime) + 0.5 ||
         Math.floor(video.currentTime) < Math.floor(otherTime) - 0.5) {
@@ -121,28 +41,13 @@ class Video extends React.Component {
     }
   }
 
-  emitPlayAndListenForPause(eventOrVideo) {
-    const video = eventOrVideo.target ? eventOrVideo.target : eventOrVideo;
-    this.props.socket.emit('play', video.currentTime);
-  }
-
-  emitPauseAndListenForPlay(eventOrVideo) {
-    const video = eventOrVideo.target ? eventOrVideo.target : eventOrVideo;
-    this.props.socket.emit('pause', video.currentTime);
-  }
-
-  emitGoBack(video) {
-    // this method will only ever be passed a video, never an event
-    this.props.socket.emit('go back', video.currentTime);
-  }
-
   render() {
     return (
       <div className="video-container">
         <div className="video-border"></div>
         <video
-          onPlay={this.emitPlayAndListenForPause}
-          onPause={this.emitPauseAndListenForPlay}
+          onPlay={this.props.emitPlayAndListenForPause}
+          onPause={this.props.emitPauseAndListenForPlay}
           className="video"
           controls
         >
--- client/components/VideoWrapper.jsx
+++ client/components/VideoWrapper.jsx
@@ -15,6 +15,10 @@ class VideoWrapper extends Component {
     this.state = {
       youtubeLink: props.youtubeLink
     };
+
+    this.initVoice = this.initVoice.bind(this);
+    this.emitPlayAndListenForPause = this.emitPlayAndListenForPause.bind(this);
+    this.emitPauseAndListenForPlay = this.emitPauseAndListenForPlay.bind(this);
   }
 
   componentDidMount() {
@@ -67,12 +71,109 @@ class VideoWrapper extends Component {
     });
   }
 
+  // Initialize annyong voice command library and define commands
+  initVoice(video) {
+    var playFunction = () => {
+      console.log('play voice command received');
+      video.play();
+      this.emitPlayAndListenForPause(video);
+    };
+
+    var pauseFunction = () => {
+      console.log('pause voice command received');
+      video.pause();
+      this.emitPauseAndListenForPlay(video);
+    };
+
+    var goBackFunction = () => {
+      console.log('go back voice command received');
+      video.currentTime = Math.floor(video.currentTime - 10, 0);
+      this.emitGoBack(video);
+    };
+
+    var muteFunction = () => {
+      console.log('mute voice command received');
+      video.muted = true;
+    };
+
+    var unmuteFunction = () => {
+      console.log('unmute voice command received');
+      video.muted = false;
+    };
+
+    var playCommands = {'play': playFunction};
+    var goBackCommands = {'go back': goBackFunction};
+
+    // define words that empirically sound like our main command, and synonyms
+    var pauseWords = ['pause', 'call', 'car', 'cars', 'cause', 'hans', 
+      'hollis', 'hot', 'palm', 'paul', 'paula\'s', 'paw', 'pawn', 
+      'paws', 'pod', 'pods', 'polish', 'pond', 'posh'];
+    var muteWords = ['mute', 'new', 'news', 'newt', 'nude', 'use', 'used', 'you'];
+    var unmuteWords = ['unmute', 'enhance', 'enhanced', 'in hands', 'in hats', 'n hance'];
+
+    // generate a command object for each array of command words
+    var pauseCommands = pauseWords.reduce(function(obj, word) {
+      obj[word] = pauseFunction;
+      return obj;
+    }, {});
+
+    var muteCommands = muteWords.reduce(function(obj, word) {
+      obj[word] = muteFunction;
+      return obj;
+    }, {});
+
+    var unmuteCommands = unmuteWords.reduce(function(obj, word) {
+      obj[word] = unmuteFunction;
+      return obj;
+    }, {});
+
+
+    if (annyang) {
+      annyang.debug();
+      annyang.addCommands(playCommands);
+      annyang.addCommands(pauseCommands);
+      annyang.addCommands(goBackCommands);
+      annyang.addCommands(muteCommands);
+      annyang.addCommands(unmuteCommands);
+      annyang.start();
+
+      setInterval(function() {
+        var listen = annyang.isListening();
+        console.log('listen:', listen);
+      }, 1000);
+
+      annyang.addCallback('error', function(err) {
+        console.log('annyang error:', err);
+      });
+    }
+  }
+
+  emitPlayAndListenForPause(eventOrVideo) {
+    const video = eventOrVideo.target ? eventOrVideo.target : eventOrVideo;
+    this.props.socket.emit('play', video.currentTime);
+  }
+
+  emitPauseAndListenForPlay(eventOrVideo) {
+    const video = eventOrVideo.target ? eventOrVideo.target : eventOrVideo;
+    this.props.socket.emit('pause', video.currentTime);
+  }
+
+  emitGoBack(video) {
+    // this method will only ever be passed a video, never an event
+    this.props.socket.emit('go back', video.currentTime);
+  }
+
   render() {
     return (
       <div>
         {this.props.videoType === 'file' ?
           <div className="wrapper">
-            <Video socket={this.props.socket} />
+            <Video 
+              socket={this.props.socket} 
+              initVoice={this.initVoice} 
+              emitPlayAndListenForPause={this.emitPlayAndListenForPause} 
+              emitPauseAndListenForPlay={this.emitPauseAndListenForPlay} 
+            />
             <ChatSpace socket={this.props.socket} isSource={this.props.isSource} peerId={this.props.peerId} />
           </div> :
           <div className="wrapper">
